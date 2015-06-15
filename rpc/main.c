f86e48deca9da2551bba65e681544f0a7a50c83a
Biwott Kevin <Benoni@Biwotts-MacBook-Air.local>
2015-06-15 08:51:34 +0300

    Initial Commit

---
+++ rpc/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  rpc
+//
+//  Created by Biwott Kevin on 6/15/15.
+//  Copyright (c) 2015 Biwott Kevin. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
